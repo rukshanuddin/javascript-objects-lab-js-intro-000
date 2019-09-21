a54c8dee0c5d10cff12dafcba0693e7d29d7bcbb
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-09-21 18:34:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var recipes ={}
 
 function updateObjectWithKeyAndValue(object, key, value){
 
-  return Object.assign({}, object {[key] : value})
-  
+  return Object.assign({}, object, {[key] : value})
+
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
